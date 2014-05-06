080b7e81457afac2f63a3d3d8e937a22ad47e28d
[Timothy Chin] <tim@DebianVM.InternalNet>
2014-05-06 22:31:26 +1000

    Added runGame.c for the runGame() function activity, in addition to including what bits need to do what so someone can actually write it.

---
+++ runGame.c
@@ -0,0 +1,35 @@
+/* runGame.c - interactive frontend for the game
+ * By Timothy Chin, James Houlahan, Carl Liu and Matthew Siesco
+ * 
+ * Runs a simulation of the game, but takes in user input instead of
+ * calling AI functions. Can be used for debugging.
+ * 
+ * Action codes will be determined by scanf's
+ * 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Game.h"
+
+int main (int argc, char *argv[]){
+
+   printf("Initialising printfs");
+   // initialising code
+
+   while (gameEnd == FALSE);
+
+      throwDice(random number);
+
+         while (action != 0);
+         printf("Current turn + stats")
+         printf("Please input your action code")
+
+         execute(actioncode);
+      }
+   }
+
+   printf(endgame stats + winner)
+
+   return EXIT_SUCCESS;
+}
