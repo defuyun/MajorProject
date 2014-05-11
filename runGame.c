20a34cdec033a1821286e960b88f4304802457a7
sheomualjy <sheomualjy@outlook.com>
2014-05-11 16:18:46 +1000

    added runGame.c that was submitted to openlearning

--- runGame.c
+++ runGame.c
@@ -10,26 +10,112 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include "Game.h"
 
+
+#define DEFAULT_DISCIPLINES { \
+    STUDENT_BQN,    STUDENT_MMONEY, STUDENT_MJ, \
+    STUDENT_MMONEY, STUDENT_MJ,     STUDENT_BPS, \
+    STUDENT_MTV,    STUDENT_MTV,    STUDENT_BPS, \
+    STUDENT_MTV,    STUDENT_BQN,    STUDENT_MJ, \
+    STUDENT_BQN,    STUDENT_THD,    STUDENT_MJ, \
+    STUDENT_MMONEY, STUDENT_MTV,    STUDENT_BQN, \
+    STUDENT_BPS }
+
+#define DEFAULT_DICE { \
+    9, 10,  8, 12,  6,  5,  \
+    3, 11,  3, 11,  4,  6, \
+    4,  9,  9,  2,  8, 10, \
+    5 }
+
+
+// ask the user for an action
+action getAction(void);
+void printResources(Game g, int player);
+
+
+// ask each player for an action
 int main (int argc, char *argv[]){
+    // seed the random number generator
+    srand(time(NULL));
 
-   printf("Initialising printfs");
-   // initialising code
+    // get a new game
+    int disciplines[] = DEFAULT_DISCIPLINES;
+    int dice[] = DEFAULT_DICE;
+    Game g = newGame(disciplines, dice);
 
-   while (gameEnd == FALSE);
+    int hasWinner = FALSE;
+    int winner = NO_ONE;
+    while (hasWinner == FALSE) {
+        throwDice(g, rand()%12 + 2);
+        printf("Player %d's turn\n", getWhoseTurn(g));
+        printResources(g, getWhoseTurn(g));
 
-      throwDice(random number);
+        // get the player's action
+        action a = getAction();
+        while (isLegalAction(g, a) == FALSE) {
+            printf("Illegal action\n");
+            a = getAction();
+        }
 
-         while (action != 0);
-         printf("Current turn + stats")
-         printf("Please input your action code")
+        // if they wanted a spinoff, calculate the chance outcome
+        if (a.actionCode == START_SPINOFF) {
+            if (rand()%3 == 0) {
+                a.actionCode = OBTAIN_IP_PATENT;
+            } else {
+                a.actionCode = OBTAIN_PUBLICATION;
+            }
+        }
 
-         execute(actioncode);
-      }
-   }
+        // make their action and check if they won
+        makeAction(g, a);
+        if (getKPIpoints(g, getWhoseTurn(g)) >= 150) {
+            hasWinner = TRUE;
+            winner = getWhoseTurn(g);
+        }
+    }
 
-   printf(endgame stats + winner)
+    printf("Player %d won\n", winner);
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
+}
+
+
+// ask the user for an action
+action getAction(void) {
+    action a;
+
+    printf("Enter an action to perform: ");
+    scanf("%d", &(a.actionCode));
+
+    // depending on the action, get more information
+    if (a.actionCode == BUILD_CAMPUS || a.actionCode == OBTAIN_ARC
+            || a.actionCode == BUILD_GO8) {
+        printf("Enter a path: ");
+        path p;
+        scanf("%s", p);
+        strcpy(a.destination, p);
+    } else if (a.actionCode == RETRAIN_STUDENTS) {
+        printf("Enter student type to retrain from: ");
+        scanf("%d", &(a.disciplineFrom));
+        printf("Enter student type to retrain to: ");
+        scanf("%d", &(a.disciplineTo));
+    }
+
+    return a;
+}
+
+
+// print current resources tally
+void printResources(Game g, int player) {
+    printf("Player %d has resources:\n", getWhoseTurn(g));
+    printf("\t%d THD\n\t%d BPS\n\t%d BQN\n\t%d MJ\n\t%d MTV\n\t%d "
+            "MMONEY\n", getStudents(g, player, STUDENT_THD),
+            getStudents(g, player, STUDENT_BPS),
+            getStudents(g, player, STUDENT_BQN),
+            getStudents(g, player, STUDENT_MJ),
+            getStudents(g, player, STUDENT_MTV),
+            getStudents(g, player, STUDENT_MMONEY));
 }
